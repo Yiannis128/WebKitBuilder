db3097a3d793ffaac88d297913f63196d40c46fc
Yiannis128 <yiannis128@hotmail.com>
2019-10-22 21:00:19 +0100

    Created script framework. Now to code it.

--- build.ps1
+++ build.ps1
@@ -20,11 +20,20 @@ $Version = "0.1";
 $TAG_START = ".*@(";
 $TAG_END = ".*)@.*";
 
-$IGNORE_DIRECTORIES = "Build", "Import";
-$IGNORE_FILES = ".git", "README.md", "build.ps1";
+$IGNORE_DIRECTORIES = "Build", "Import", ".git";
+$IGNORE_FILES = "README.md", "build.ps1";
 $IGNORE_ITEMS = $IGNORE_DIRECTORIES + $IGNORE_FILES; #Powershell allows list appending.
 
-#
+$PARSED_EXTENTIONS = ".html", ".css";
+
+$BuildLocation = "Build" + (GetDirSeparator);
+
+#This method scans through the directory pointed to by $directoryInfo.
+#The method then gets all the files and checks their extention.
+#If they have an extention of $PARSED_EXTENTIONS then they
+#will be passed to the ParseFile method which will parse and return
+#the results.
+#The files will regardless if parsed or not be saved to the $Build folder.
 function BuildDirectoryRecursive
 {
   param ([System.IO.DirectoryInfo] $directoryInfo)
@@ -49,10 +58,10 @@ function GetTransformedRelativeDirectoryInBuildFolder
 #Scans through the file line by line. Tries to find a tag which is described by $TAG_START
 #and ends with $TAG_END. In between there exists a file name that describes a file that
 #should reside inside the ./Imports folder, the method then replaces the tag with the content
-#of that file and then saves the file by calling the SaveFile method. 
+#of that file and then returns the file content. 
 function ParseFile
 {
-  param ()
+  param ([string] $fileContent)
   process
   {
 
@@ -67,6 +76,23 @@ function SaveFile
   process
   {
 
+  }
+}
+
+#Gets the respective directory separator of the current OS.
+function GetDirSeparator
+{
+  param()
+  process
+  {
+    if($IsLinux -or $IsMacOs)
+    {
+      return "/";
+    }
+    else
+    {
+      return "\";
+    }
   }
 }
 
